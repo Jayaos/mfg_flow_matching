19bf1fff58bca3dae89dedf301e113b3d7d25bd7
Jay Lee <jayaos1002@gmail.com>
2026-02-25 13:20:06 -0500

    update np

--- mfg_flow_nonpotential.py
+++ mfg_flow_nonpotential.py
@@ -8,23 +8,22 @@ from torchdiffeq import odeint
 from scipy.stats import gaussian_kde
 
 
-def run_mfg_nonpotential(num_outer_loop, particle_num, num_timestep, ode_solver,
-                particle_optimization_training_step, step_size, intermediate_update_frequency, 
-                hidden_dims, velocity_field_training_step, velocity_field_learning_rate,
-                mu_0, cov_matrix_0, a, lamb_F, mu_1, lamb_G):
+def run_mfg_nonpotential(num_outer_loop, particle_num, ode_timesteps, ode_solver,
+                         particle_steps, step_size, cost_update_frequency, 
+                         vf_hidden_dims, vf_steps, vf_learning_rate,
+                         mu_0, cov_matrix_0, a, lamb_F, mu_1, lamb_G):
     """
-    
     Args:
         num_outer_loop: Number of outer loop iterations
         particle_num: Number of particles
-        num_timestep: Number of time steps
+        ode_timesteps: Number of time steps for ODE
         ode_solver: ODE solver to use ("euler" or "rk4")
-        particle_optimization_training_step: Number of particle optimization steps
+        particle_steps: Number of particle optimization steps
         step_size: Step size for particle optimization
-        intermediate_update_frequency: Frequency of intermediate updates
-        hidden_dims: Hidden dimensions for neural network
-        velocity_field_training_step: Number of velocity field training steps
-        velocity_field_learning_rate: Learning rate for velocity field
+        cost_update_frequency: Frequency of intermediate updates
+        vf_hidden_dims: Hidden dimensions for neural network
+        vf_steps: Number of velocity field training steps
+        vf_learning_rate: Learning rate for velocity field
         mu_0: Initial mean
         cov_matrix_0: Initial covariance matrix
         a: Parameter vector
@@ -39,10 +38,10 @@ def run_mfg_nonpotential(num_outer_loop, particle_num, num_timestep, ode_solver,
     """
     # setup
     initial_gaussian_dist = torch.distributions.MultivariateNormal(loc=mu_0, covariance_matrix=cov_matrix_0)  
-    velocity_field = MLPVelocityField(2, 1, hidden_dims, "concatlinear", activation=torch.nn.ReLU())
-    vf_optim = torch.optim.Adam(velocity_field.parameters(), lr=velocity_field_learning_rate)
-    timesteps = torch.linspace(0, 1, num_timestep)
-    delta_t = 1/(num_timestep-1)
+    velocity_field = MLPVelocityField(2, 1, vf_hidden_dims, "concatlinear", activation=torch.nn.ReLU())
+    vf_optim = torch.optim.Adam(velocity_field.parameters(), lr=vf_learning_rate)
+    timesteps = torch.linspace(0, 1, ode_timesteps)
+    delta_t = 1/(ode_timesteps-1)
 
     residuals = []
 
@@ -53,7 +52,7 @@ def run_mfg_nonpotential(num_outer_loop, particle_num, num_timestep, ode_solver,
         if k == 0:
             # initialization of trajectory: zero velocity field
             x_init = x_init.unsqueeze(1) # (particle_num, 1, dim)
-            x_trajectory = x_init.repeat(1, num_timestep, 1) # (particle_num, num_timestep, dim)
+            x_trajectory = x_init.repeat(1, ode_timesteps, 1) # (particle_num, num_timestep, dim)
             # # initialization of trajectory: random initialization
             # x_init = x_init.unsqueeze(1) # (particle_num, 1, dim)
             # x_trajectory = torch.randn(particle_num, num_timestep, 2) * 0.1 + mu_0 # (particle_num, num_timestep, dim)
@@ -67,7 +66,7 @@ def run_mfg_nonpotential(num_outer_loop, particle_num, num_timestep, ode_solver,
             # plt.axis('equal')
             # plt.show()
         else:
-            (particle_num, num_timestep, 2)
+            (particle_num, ode_timesteps, 2)
             with torch.no_grad():
                 x_trajectory = odeint(velocity_field, x_init, timesteps, method=ode_solver)
             x_trajectory = x_trajectory.permute(1,0,2) # (particle_num, num_timestep, dim)
@@ -78,7 +77,7 @@ def run_mfg_nonpotential(num_outer_loop, particle_num, num_timestep, ode_solver,
 
         # particle optimization
         residuals_particle_optimization = []
-        for j in range(particle_optimization_training_step):
+        for j in range(particle_steps):
             with torch.no_grad():
                 # (particle_num, num_timestep-2)
                 DttX = (x_trajectory[:,:-2,:] - 2*x_trajectory[:,1:-1,:] + x_trajectory[:,2:,:]) * (1/delta_t**2) # pay attention
@@ -100,7 +99,7 @@ def run_mfg_nonpotential(num_outer_loop, particle_num, num_timestep, ode_solver,
     
                 residuals_particle_optimization.append(res)
 
-                if (j+1) % intermediate_update_frequency == 0:
+                if (j+1) % cost_update_frequency == 0:
                     # compute exp(a dot x) of the particle trajectory
                     exp_term = torch.exp( torch.matmul(x_trajectory[:,1:-1,:], a) ).unsqueeze(-1) # (particle_num, num_timestep-1)
                     mean_exp_term = torch.mean(exp_term, dim=0) # (num_timestep-1, )
@@ -110,7 +109,7 @@ def run_mfg_nonpotential(num_outer_loop, particle_num, num_timestep, ode_solver,
 
         # update velocity field
         loss_sum = 0.
-        for _ in range(velocity_field_training_step):
+        for _ in range(vf_steps):
 
             loss = vf_loss_fn(velocity_field, x_trajectory, timesteps, delta_t)
             vf_optim.zero_grad()
@@ -118,7 +117,7 @@ def run_mfg_nonpotential(num_outer_loop, particle_num, num_timestep, ode_solver,
             vf_optim.step()
             loss_sum += loss["loss"].item()
 
-        print("avg vf loss at outer loop {}: {}".format(k+1, loss_sum/velocity_field_training_step))
+        print("avg vf loss at outer loop {}: {}".format(k+1, loss_sum/vf_steps))
 
         with torch.no_grad():
             x_trajectory_final = odeint(velocity_field, x_trajectory[:,0,:], timesteps, method=ode_solver)
@@ -139,52 +138,51 @@ def parse_arguments():
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
     
-    # Main parameters
-    parser.add_argument('--K', type=int, default=100,
+    # data / problem
+    parser.add_argument('--outer_loop', type=int, default=100,
                         help='Number of outer loop iterations')
     parser.add_argument('--n', type=int, default=1000,
                         help='Number of particles')
-    parser.add_argument('--m', type=int, default=21,
+    parser.add_argument('--ode_timesteps', type=int, default=21,
                         help='Number of time steps')
-    parser.add_argument('--ode-solver', type=str, default='euler',
+    parser.add_argument('--ode_solver', type=str, default='euler',
                         choices=['euler', 'rk4'],
                         help='ODE solver to use')
+    parser.add_argument('--no_plot', action='store_true',
+                        help='Disable plotting')
+    parser.add_argument('--save_plots', type=str, default="./results/mfg_flow_nonpotential/",
+                        help='Directory to save plots (if not specified, plots are displayed)')
+    parser.add_argument('--outer_batch', type=int, default=5000,
+                        help='Number of particles for resampling')
     
     # Non-potential game parameters (default values from cell 3)
-    parser.add_argument('--mu-0', type=float, nargs=2, default=[0.0, 1.0],
+    parser.add_argument('--mu_0', type=float, nargs=2, default=[0.0, 1.0],
                         help='Initial mean (2 values)')
-    parser.add_argument('--cov-0-diag', type=float, nargs=2, default=[0.02, 0.1],
+    parser.add_argument('--cov_0_diag', type=float, nargs=2, default=[0.02, 0.1],
                         help='Diagonal of initial covariance matrix (2 values)')
     parser.add_argument('--a', type=float, nargs=2, default=[0.0, 1.0],
                         help='Parameter vector a (2 values)')
-    parser.add_argument('--lamb-F', type=float, default=10.0,
+    parser.add_argument('--lamb_F', type=float, default=10.0,
                         help='Force parameter lambda_F')
-    parser.add_argument('--mu-1', type=float, default=-1.0,
+    parser.add_argument('--mu_1', type=float, default=-1.0,
                         help='Target mean mu_1')
-    parser.add_argument('--lamb-G', type=float, default=1.0,
+    parser.add_argument('--lamb_G', type=float, default=1.0,
                         help='Goal parameter lambda_G')
     
-    # Training parameters (default values from cell 4)
-    parser.add_argument('--particle-optim-steps', type=int, default=100,
+    # training
+    parser.add_argument('--particle_steps', type=int, default=100,
                         help='Number of particle optimization steps')
-    parser.add_argument('--step-size', type=float, default=0.01,
-                        help='Step size for particle optimization')
-    parser.add_argument('--intermediate-update-freq', type=int, default=2,
-                        help='Intermediate update frequency')
-    parser.add_argument('--hidden-dims', type=int, nargs='+', default=[4, 8, 16],
+    parser.add_argument('--step_size', type=float, default=0.01,
+                        help='step size for particle optimization')
+    parser.add_argument('--cost_update_frequency', type=int, default=2,
+                        help='cost update frequency')
+    parser.add_argument('--vf_hidden_dims', type=int, nargs='+', default=[4, 8, 16],
                         help='Hidden dimensions for neural network')
-    parser.add_argument('--vf-steps', type=int, default=100,
+    parser.add_argument('--vf_steps', type=int, default=100,
                         help='Number of velocity field training steps')
-    parser.add_argument('--vf-lr', type=float, default=0.01,
+    parser.add_argument('--vf_learning_rate', type=float, default=0.01,
                         help='Velocity field learning rate')
     
-    # Plotting and output options
-    parser.add_argument('--no-plot', action='store_true',
-                        help='Disable plotting')
-    parser.add_argument('--save-plots', type=str, default="./results/mfg_flow_nonpotential/",
-                        help='Directory to save plots (if not specified, plots are displayed)')
-    parser.add_argument('--n-resample', type=int, default=5000,
-                        help='Number of particles for resampling')
     
     return parser.parse_args()
 
@@ -197,9 +195,9 @@ def main():
     print("Non-potential game with interaction cost defined by asymmetric kernel")
     print("=" * 60)
     print(f"\nParameters:")
-    print(f"  K (outer loops): {args.K}")
+    print(f"  outer loops: {args.outer_loop}")
     print(f"  n (particles): {args.n}")
-    print(f"  m (timesteps): {args.m}")
+    print(f"  m (timesteps): {args.ode_timesteps}")
     print(f"  ODE solver: {args.ode_solver}")
     print(f"  mu_0: {args.mu_0}")
     print(f"  cov_0_diag: {args.cov_0_diag}")
@@ -221,9 +219,9 @@ def main():
     lamb_G = args.lamb_G
     
     v, x, results = run_mfg_nonpotential(
-        args.K, args.n, args.m, args.ode_solver,
-        args.particle_optim_steps, args.step_size, args.intermediate_update_freq,
-        args.hidden_dims, args.vf_steps, args.vf_lr,
+        args.outer_loop, args.n, args.ode_timesteps, args.ode_solver,
+        args.particle_steps, args.step_size, args.cost_update_frequency,
+        args.vf_hidden_dims, args.vf_steps, args.vf_learning_rate,
         mu_0, cov_matrix_0, a, lamb_F, mu_1, lamb_G
     )
     print("Done!")
@@ -231,12 +229,12 @@ def main():
     residuals = results["residuals"]
     
     # Resample and compute trajectory
-    print(f"\nResampling with {args.n_resample} particles...")
-    timesteps = torch.linspace(0, 1, args.m)
+    print(f"\nResampling with {args.outer_batch} particles...")
+    timesteps = torch.linspace(0, 1, args.ode_timesteps)
     initial_gaussian_dist = torch.distributions.MultivariateNormal(
         loc=mu_0, covariance_matrix=cov_matrix_0
     )
-    x_init_resample = initial_gaussian_dist.sample([args.n_resample])
+    x_init_resample = initial_gaussian_dist.sample([args.outer_batch])
     with torch.no_grad():
         x_resample = odeint(v, x_init_resample, timesteps, method=args.ode_solver)
     x_resample = x_resample.permute(1, 0, 2)  # (n_resample, num_timestep, dim)
@@ -248,10 +246,10 @@ def main():
         # Plot 1: Particle trajectories
         plt.figure(figsize=(5, 3))
         num_traj = 10
-        colors = plt.cm.viridis(np.linspace(1, 0, args.m))
-        sizes = np.linspace(20, 10, args.m)
-        scatter_indices = [0, 5, 10, 15, 20] if args.m >= 21 else list(range(args.m))
-        scatter_indices = [i for i in scatter_indices if i < args.m]
+        colors = plt.cm.viridis(np.linspace(1, 0, args.ode_timesteps))
+        sizes = np.linspace(20, 10, args.ode_timesteps)
+        scatter_indices = [0, 5, 10, 15, 20] if args.ode_timesteps >= 21 else list(range(args.ode_timesteps))
+        scatter_indices = [i for i in scatter_indices if i < args.ode_timesteps]
         
         for i in range(num_traj):
             plt.plot(x[i, :, 0].numpy(), x[i, :, 1].numpy(), color='#1f77b4', alpha=1)
@@ -260,7 +258,7 @@ def main():
                 if i == 0:
                     if j == 0:
                         label = 't=0'
-                    elif j == args.m - 1:
+                    elif j == args.ode_timesteps - 1:
                         label = 't=1'
                     else:
                         label = f't={timesteps[j]:.2f}'
@@ -283,7 +281,7 @@ def main():
         
         # Plot 2: Residuals
         plt.figure(figsize=(4, 3))
-        plt.plot(range(1, args.K + 1), residuals)
+        plt.plot(range(1, args.outer_loop + 1), residuals)
         plt.yscale("log")
         plt.xlabel("Epoch $k$")
         plt.ylabel("Residual")
@@ -298,7 +296,7 @@ def main():
         plt.close()
         
         # Plot 3: Density evolution
-        t_indices = np.linspace(0, args.m - 1, 8, dtype=int)
+        t_indices = np.linspace(0, args.ode_timesteps - 1, 8, dtype=int)
         fig, axes = plt.subplots(1, len(t_indices), figsize=(14, 2), sharex=True, sharey=True)
         region = [-0.6, 0.6, -1.2, 1.8]
         x1, x2 = np.mgrid[region[0]:region[1]:200j, region[2]:region[3]:200j]
